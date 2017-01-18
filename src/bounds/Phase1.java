529e73adf7f83c8f478c40c6cafa755f5e6cb052
nfs13a <nfs13a@acu.edu>
2017-01-17 22:34:16 -0600

    added stuff, sick so dont remember

--- src/bounds/Phase1.java
+++ src/bounds/Phase1.java
@@ -1,26 +1,38 @@
 package bounds;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 class globe {
 	
+	private String name;
 	private double value;
 	private double cost;
 	
-	public globe(double v, double c) {
+	public globe(String n, double v, double c) {
+		name = n;
 		value = v;
 		cost = c;
 	}
 	
-	double getValue() {
+	public String getName() {
+		return name;
+	}
+	
+	public double getValue() {
 		return value;
 	}
 	
-	double getCost() {
+	public double getCost() {
 		return cost;
 	}
 	
-	double getRatio() {
+	public double getRatio() {
 		return value / cost;
 	}
 }
@@ -61,6 +73,41 @@ class PQsortRatio implements Comparator<globe> {
 public class Phase1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int capacity;
+		PriorityQueue<globe> value = new PriorityQueue<globe>(1, new PQsortValue());
+		PriorityQueue<globe> cost = new PriorityQueue<globe>(1, new PQsortCost());
+		PriorityQueue<globe> ratio = new PriorityQueue<globe>(1, new PQsortRatio());
+		
+		try {
+			// sets up parsing data
+			BufferedReader br = null;
+			String line = "";
+			
+			//gets current directory to know where to find csv's
+			String currentDir = new File("").getAbsolutePath();
+			
+			try {
+				br = new BufferedReader(new FileReader(currentDir + "\\k05.csv"));
+				capacity = Integer.parseInt(br.readLine());
+				System.out.println("capacity: " + capacity);
+				while ((line = br.readLine()) != null) {
+					String[] itemInfo = line.split(",");	//name,cost,value
+					System.out.println(itemInfo[0] + ":" + itemInfo[1] + ":" + itemInfo[2]);
+					value.add(new globe(itemInfo[0], Integer.parseInt(itemInfo[2]), Integer.parseInt(itemInfo[1])));
+					cost.add(new globe(itemInfo[0], Integer.parseInt(itemInfo[2]), Integer.parseInt(itemInfo[1])));
+					ratio.add(new globe(itemInfo[0], Integer.parseInt(itemInfo[2]), Integer.parseInt(itemInfo[1])));
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			br.close();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
