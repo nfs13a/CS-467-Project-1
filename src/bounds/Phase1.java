835818c0f8206d29732d39407c7fbfdaa78dbb41
nfs13a <nfs13a@acu.edu>
2017-01-18 20:16:19 -0600

    new input, actual output
    -added the other 3 input files
    -produce output of highest lower bound
    -produce output of upper bound

--- src/bounds/Phase1.java
+++ src/bounds/Phase1.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Vector;
 
 class globe {
 	
@@ -40,9 +41,9 @@ class globe {
 class PQsortValue implements Comparator<globe> {
 	public int compare(globe one, globe two) {
 		if (one.getValue() > two.getValue()) {
-			return 1;
-		} else if (one.getValue() < two.getValue()) {
 			return -1;
+		} else if (one.getValue() < two.getValue()) {
+			return 1;
 		}
 		return 0;
 	}
@@ -62,9 +63,9 @@ class PQsortCost implements Comparator<globe> {
 class PQsortRatio implements Comparator<globe> {
 	public int compare(globe one, globe two) {
 		if (one.getRatio() > two.getRatio()) {
-			return 1;
-		} else if (one.getRatio() < two.getRatio()) {
 			return -1;
+		} else if (one.getRatio() < two.getRatio()) {
+			return 1;
 		}
 		return 0;
 	}
@@ -73,7 +74,7 @@ class PQsortRatio implements Comparator<globe> {
 public class Phase1 {
 
 	public static void main(String[] args) {
-		int capacity;
+		int capacity = 0;
 		PriorityQueue<globe> value = new PriorityQueue<globe>(1, new PQsortValue());
 		PriorityQueue<globe> cost = new PriorityQueue<globe>(1, new PQsortCost());
 		PriorityQueue<globe> ratio = new PriorityQueue<globe>(1, new PQsortRatio());
@@ -108,6 +109,78 @@ public class Phase1 {
 			e.printStackTrace();
 		}
 		
+		double totalFromValue = 0;
+		double capacityFromValue = 0;
+		Vector<globe> itemsTakenByValue = new Vector<>();
+		do {
+			String itemName = value.peek().getName();
+			double tempVal = value.peek().getValue();
+			double tempRatio = value.peek().getRatio();
+			double tempCost = value.poll().getCost();
+			System.out.println(itemName + ":" + tempVal + ":" + tempCost + ":" + tempRatio);
+			if (capacityFromValue + tempCost <= capacity) {
+				totalFromValue += tempVal;
+				capacityFromValue += tempCost;
+				itemsTakenByValue.add(new globe(itemName, tempVal, tempCost));
+				System.out.println("taken");
+			}
+		} while (!value.isEmpty());
 		
+		
+		System.out.println(totalFromValue);
+		System.out.println(capacityFromValue);
+		for (globe g : itemsTakenByValue) {
+			System.out.println(g.getName());
+		}
+		
+		double totalFromCost = 0;
+		double capacityFromCost = 0;
+		Vector<globe> itemsTakenByCost = new Vector<>();
+		while (totalFromCost + cost.peek().getCost() <= capacity) {
+			itemsTakenByCost.add(new globe(cost.peek().getName(), cost.peek().getValue(), cost.peek().getCost()));
+			totalFromCost += cost.peek().getValue();
+			capacityFromCost += cost.poll().getCost();
+		}
+		
+		System.out.println(totalFromCost);
+		System.out.println(capacityFromCost);
+		for (globe g : itemsTakenByCost) {
+			System.out.println(g.getName());
+		}
+		
+		double upperBound = 0;
+		double totalFromRatio = 0;
+		double capacityFromRatio = 0;
+		Vector<globe> itemsTakenByRatio = new Vector<>();
+		while (totalFromRatio + ratio.peek().getCost() <= capacity) {
+			itemsTakenByRatio.add(new globe(ratio.peek().getName(), ratio.peek().getValue(), ratio.peek().getCost()));
+			totalFromRatio += ratio.peek().getValue();
+			capacityFromRatio += ratio.poll().getCost();
+		}
+		upperBound = totalFromRatio + ratio.poll().getValue();
+		
+		System.out.println(totalFromRatio);
+		System.out.println(capacityFromRatio);
+		for (globe g : itemsTakenByRatio) {
+			System.out.println(g.getName());
+		}
+		
+		
+		double lowerBound;
+		if (totalFromValue >= totalFromCost) {
+			if (totalFromValue >= totalFromRatio) {
+				lowerBound = totalFromValue;
+			} else {
+				lowerBound = totalFromRatio;
+			}
+		} else {
+			if (totalFromCost >= totalFromRatio) {
+				lowerBound = totalFromCost;
+			} else {
+				lowerBound = totalFromRatio;
+			}
+		}
+		System.out.println("Lower bound: " + lowerBound);
+		System.out.println("Upper bound: " + upperBound);
 	}
 }
